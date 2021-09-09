53c1fb7e5cd3d6994c91fd0a01c2d98b5b7b583d
EvitaU <evita.udre@gmail.com>
2021-09-09 23:11:58 +0300

    database update

--- src/main/resources/movieticketsystem/database.sql
+++ src/main/resources/movieticketsystem/database.sql
@@ -1,26 +1,47 @@
-drop database if exists MovieApp;
+drop database if exists MovieBooking;
 SET GLOBAL time_zone = '+3:00';
 
-create database if not exists MovieApp;
+create database if not exists MovieBooking;
 
-use MovieApp;
+use MovieBooking;
 
 create table if not exists movies(
-	 id int not null auto_increment,
-     title varchar(50) not null,
-     genre varchar(50) not null,
-     released_at year not null,
-     primary key(id)
-);
+    id int not null auto_increment,
+    title varchar(50) not null,
+    genre varchar(50) not null,
+    released_at year not null,
+    status varchar(20),
+    primary key(id)
+    );
 
 create table if not exists users(
-     username varchar(50) not null,
-     firstName varchar(50) not null,
-     lastName varchar(50) not null,
-     admin boolean
+    id int not null auto_increment,
+    username varchar(50) not null,
+    password text not null,
+    firstName varchar(50) not null,
+    lastName varchar(50) not null,
+    admin boolean,
+    primary key(id)
+    );
+create table if not exists rooms(
+    id int not null auto_increment,
+    number int,
+    primary key(id)
+    );
 
-);
+create table if not exists bookings(
+    id int not null auto_increment,
+    movie_id int not null,
+    room_id int not null,
+    date varchar (20),
+    primary key(id),
+    foreign key(movie_id) references movies(id),
+    foreign key(room_id) references rooms(id)
+    );
 
-create table if not exists booking
-
-SELECT * FROM movies;
+create table if not exists screenings(
+    id int not null auto_increment,
+    date varchar(50) not null,
+    movie_id int not null,
+    foreign key(id) references movies(id)
+    );
