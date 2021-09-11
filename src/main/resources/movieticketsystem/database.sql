70e524fe66413647623653ea16228c8d0411e81c
EvitaU <evita.udre@gmail.com>
2021-09-11 13:25:51 +0300

    database update

--- src/main/resources/movieticketsystem/database.sql
+++ src/main/resources/movieticketsystem/database.sql
@@ -7,41 +7,41 @@ use MovieBooking;
 
 create table if not exists movies(
     id int not null auto_increment,
-    title varchar(50) not null,
+    name varchar(50) not null,
     genre varchar(50) not null,
-    released_at year not null,
-    status varchar(20),
     primary key(id)
     );
 
 create table if not exists users(
-    id int not null auto_increment,
-    username varchar(50) not null,
-    password text not null,
     firstName varchar(50) not null,
     lastName varchar(50) not null,
-    admin boolean,
-    primary key(id)
+    username varchar(50) not null,
+    password text not null,
+    isAdmin boolean,
+    primary key(username)
     );
 create table if not exists rooms(
-    id int not null auto_increment,
     number int,
-    primary key(id)
+    primary key(number)
     );
 
 create table if not exists bookings(
     id int not null auto_increment,
-    movie_id int not null,
-    room_id int not null,
-    date varchar (20),
+    movie_id int,
+    roomNo int,
+    date timestamp,
     primary key(id),
     foreign key(movie_id) references movies(id),
-    foreign key(room_id) references rooms(id)
+    foreign key(roomNo) references rooms(number)
     );
 
 create table if not exists screenings(
     id int not null auto_increment,
-    date varchar(50) not null,
-    movie_id int not null,
-    foreign key(id) references movies(id)
+    date timestamp,
+    attendance int,
+    roomNo int,
+    movie_id int,
+    primary key(id),
+    foreign key(movie_id) references movies(id),
+    foreign key(roomNo) references rooms(number)
     );
