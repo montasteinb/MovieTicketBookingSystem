fca52bf7828fab5675d743fd80e7e9b20f70f773
EvitaU <evita.udre@gmail.com>
2021-09-13 04:00:19 +0300

    database update

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -5,11 +5,19 @@ create database if not exists MovieBooking;
 
 use MovieBooking;
 
+create table if not exists rooms(
+    number int,
+    capacity int,
+    primary key(number)
+    );
+
 create table if not exists movies(
     id int not null auto_increment,
     name varchar(50) not null,
     genre varchar(50) not null,
-    primary key(id)
+    roomNo int,
+    primary key(id),
+    foreign key(roomNo) references rooms(number)
     );
 
 create table if not exists users(
@@ -20,24 +28,11 @@ create table if not exists users(
     isAdmin boolean,
     primary key(username)
     );
-create table if not exists rooms(
-    number int,
-    primary key(number)
-    );
 
-create table if not exists bookings(
-    id int not null auto_increment,
-    movie_id int,
-    roomNo int,
-    date timestamp,
-    primary key(id),
-    foreign key(movie_id) references movies(id),
-    foreign key(roomNo) references rooms(number)
-    );
 
 create table if not exists screenings(
     id int not null auto_increment,
-    date timestamp,
+    date datetime,
     attendance int,
     roomNo int,
     movie_id int,
@@ -45,3 +40,22 @@ create table if not exists screenings(
     foreign key(movie_id) references movies(id),
     foreign key(roomNo) references rooms(number)
     );
+
+create table if not exists bookings(
+    id int not null auto_increment,
+    movie_id int,
+    roomNo int,
+    username varchar(50),
+    date datetime,
+    primary key(id),
+    foreign key(movie_id) references movies(id),
+    foreign key(roomNo) references screenings(roomNo),
+    foreign key(username) references users(username)
+    );
+
+update screenings set attendance = 0 where id;
+
+INSERT INTO `moviebooking`.`rooms` (`number`) VALUES ('1');
+INSERT INTO `moviebooking`.`rooms` (`number`) VALUES ('2');
+INSERT INTO `moviebooking`.`rooms` (`number`) VALUES ('3');
+INSERT INTO `moviebooking`.`rooms` (`number`) VALUES ('4');
